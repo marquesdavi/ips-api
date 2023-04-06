3668d9fa45b6abe76512fae1d707e3fa328a4e52
marquesdavi <davibaterabatera@hotmail.com>
2023-04-05 22:33:21 -0300

    :wrench:  create database populator and configure Dockerfile

--- Backend/clients/admin.py
+++ Backend/clients/admin.py
@@ -1,3 +1,13 @@
 from django.contrib import admin
+from clients.models import Client
 
 # Register your models here.
+"""class Clients(admin.ModelAdmin):
+    list_display = ('id', 'name', 'email','cpf', 'rg', 'phone_number')
+    list_display_links = ('id', 'name')
+    search_fields = ('name', 'cpf', 'rg', 'email')
+    list_per_page = 25
+    ordering = ('name',)"""
+
+
+admin.site.register(Client)
--- Backend/setup/settings.py
+++ Backend/setup/settings.py
@@ -51,6 +51,8 @@ REST_FRAMEWORK = {
 
         'rest_framework_simplejwt.authentication.JWTAuthentication',
     ),
+    'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
+    'PAGE_SIZE': 10
 }
 
 #Simple JWT
